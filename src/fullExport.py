3c44a4310ee7ff219d04910c5c99665616789ae6
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-02-21 10:04:59 +0100

    Add new files and update existing files

---
+++ src/fullExport.py
@@ -0,0 +1,18 @@
+from extract import extract
+from transform import transform
+from load import load
+
+def __main__():
+   print("STARTING")
+   print("EXTRACTING")
+   extractData = extract()
+   
+   print("TRANSFORMING")
+   #transformData = transform(extractData) 
+
+   print("LOADING")
+   loadData = load(extractData)
+   print("DONE")
+
+if __name__== "__main__":
+    __main__()
---
+++ src/load.py
@@ -0,0 +1,43 @@
+from os import getenv
+from dotenv import find_dotenv, load_dotenv
+import boto3
+import xml.etree.ElementTree as ET
+load_dotenv(find_dotenv())
+
+S3_ENDPOINT = getenv('S3_ENDPOINT')
+S3_BUCKET = getenv('S3_BUCKET')
+S3_REGION = getenv('S3_REGION')
+S3_ACCESS_KEY = getenv('S3_ACCESS_KEY')
+S3_SECRET_ACCESS_KEY = getenv('S3_SECRET_ACCESS_KEY')
+S3_EXPORT_PATH = getenv('S3_EXPORT_PATH')
+
+def s3client():
+    session = boto3.session.Session()
+    s3_client = session.client(
+        service_name='s3',
+        aws_access_key_id=S3_ACCESS_KEY,
+        aws_secret_access_key=S3_SECRET_ACCESS_KEY,
+        endpoint_url='https://sos-'+S3_REGION+'.'+S3_ENDPOINT,
+    )
+    return s3_client
+
+import xml.etree.ElementTree as ET
+
+def puObject(data, bucket, filename):
+    s3 = s3client()
+    s3.put_object(
+        Bucket=bucket,
+        Key=filename,
+        Body=data,
+        ACL='public-read',
+        ContentType='application/json')
+    
+def loadProduct(product):
+    puObject(product, S3_BUCKET, S3_EXPORT_PATH+product['identifier']+".json")
+
+def load(products):
+    print("Loading data to target")
+    print(products)
+    for product in products:
+        print(product)
+        puObject(product, S3_BUCKET, S3_EXPORT_PATH+product['identifier']+".json")
